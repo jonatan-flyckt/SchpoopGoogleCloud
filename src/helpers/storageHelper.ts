b5265c895f43e35e22cfed8ebb4347c240ef8e35
jonatan-flyckt <jonatan.flyckt@hotmail.com>
2018-12-14 17:42:09 +0100

    Started transitioning from AWS to Google Cloud file storage.

--- src/helpers/storageHelper.ts
+++ src/helpers/storageHelper.ts
@@ -1,6 +1,14 @@
 import * as url from "url";
 import { Storage } from "@google-cloud/storage";
 import { Credentials, S3 } from "aws-sdk";
+import { Stream } from "stream";
+
+const storage = new Storage({
+	projectId: process.env.GOOGLE_CLOUD_PROJECT,
+	keyFilename: "keyfile.json"
+});
+
+const bucket = storage.bucket("schpoopstorage");
 
 const theCredentials = new Credentials(
 	process.env.S3_ACCESS_KEY_ID as string,
@@ -20,14 +28,35 @@ export const uploadImage = (
 	filePath: string,
 	contentType: string
 ) =>
-	s3
-		.upload({
-			Bucket: process.env.S3_BUCKET!,
-			Key: filePath,
-			Body: file,
-			ContentType: contentType
-		})
-		.promise();
+	new Promise<any>((resolve, reject) => {
+		const googleFile = bucket.file(filePath);
+
+		const stream = googleFile.createWriteStream({
+			metadata: {
+				contentType
+			},
+			resumable: false
+		});
+
+		stream.on("error", err => {
+			console.log(err);
+			reject(err);
+		});
+
+		stream.on("finish", () => {
+			googleFile.makePublic().then(() => {
+				resolve({ Location: getPublicUrl(filePath) });
+			});
+		});
+
+		stream.end(file);
+	});
+
+const getPublicUrl = (filename: string) => {
+	return `https://storage.googleapis.com/${
+		process.env.CLOUD_BUCKET
+	}/${filename}`;
+};
 
 export const deleteImage = (filePath: string) => {
 	return s3
@@ -37,3 +66,26 @@ export const deleteImage = (filePath: string) => {
 		})
 		.promise();
 };
+
+// export const uploadImage = (
+// 	file: Buffer,
+// 	filePath: string,
+// 	contentType: string
+// ) =>
+// 	s3
+// 		.upload({
+// 			Bucket: process.env.S3_BUCKET!,
+// 			Key: filePath,
+// 			Body: file,
+// 			ContentType: contentType
+// 		})
+// 		.promise();
+
+// export const deleteImage = (filePath: string) => {
+// 	return s3
+// 		.deleteObject({
+// 			Bucket: process.env.S3_BUCKET!,
+// 			Key: filePath
+// 		})
+// 		.promise();
+// };
